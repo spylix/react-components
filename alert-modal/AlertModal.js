566cb3f69ea5a5078f99bc30f10698c3d5263814
Paweł Stepaniuk <pawel66556655@gmail.com>
2021-02-26 20:23:42 +0100

    Updated class to functional components

--- alert-modal/AlertModal.js
+++ alert-modal/AlertModal.js
@@ -1,12 +1,11 @@
-import React, { Component } from "react";
+import React, { useEffect, useRef } from "react";
 import "./AlertModal.css";
 
-export default class AlertModal extends Component {
+export default function AlertModal(props) {
   // props:
   //  - showAlertModal - (bool) without this variable the modal won't work,
   //                     it defines whether the modal should be rendered
-  //  - toggleAlertModal - (function) example:
-  //                       toggleAlertModal = (val) => { this.setState({ showAlertModal: val }); };
+  //  - toggleAlertModal - (function), args: isOpen<bool>
   //  - title - modal title
   //  - subtitle - modal subtitle
   //  - buttons - an array of objects:
@@ -14,187 +13,174 @@ export default class AlertModal extends Component {
   //  - dontCloseAfterClick - (bool) don't close the modal after clicking on any button, if false - the modal will close.
   //                          You can use this property separately for every button (in buttons array) or for all buttons at once
 
-  // Change mobileWidth to max mobile width you choose,
+  // Change mobileWidth to max mobile viewport width you choose,
   // don't forget to change the value in css
-  mobileWidth = 1000;
+  let mobileWidth = useRef(100);
 
-  showModal;
-  firstTouchY;
+  let showModal = useRef();
+  let firstTouchY;
   // Html elements:
-  modalBody; // alert-modal-body
-  modalBg; // alert-modal-background
-  body; // document body tag
+  let modalBody = useRef(); // alert-modal-body
+  let modalBg = useRef(); // alert-modal-background
+  let body = useRef(); // document body tag
 
-  componentDidMount() {
-    // Check if modal should be open when loading the component
-    this.openModal();
-  }
+  useEffect(() => {
+    const openModal = () => {
+      // Animate the modal opening
+      modalBody.current = document.getElementById("alert-modal-body");
+      modalBg.current = document.getElementById("alert-modal-background");
+      body.current = document.getElementsByTagName("body")[0];
+      // Check props.showAlertModal (bool), the variable that decides whether the modal is open
+      // and check if props.showAlertModal has been changed (if not, there's no need for the animation)
+      if (props.showAlertModal && showModal !== props.showAlertModal) {
+        // Get the necessary elements
+        // Timeout needed for the animations to work, for elements to fully render / load
+        setTimeout(() => {
+          modalBody.current.style.transform = "";
+          modalBody.current.style.opacity = "1";
+          modalBg.current.style.opacity = "1";
+          body.current.style.overflow = "hidden";
+          // If the page is in mobile mode, remove overscroll behavior
+          // which controls refreshing the page on pull down
+          if (window.innerWidth < mobileWidth.current) {
+            body.style.overscrollBehavior = "hidden";
+          }
+        }, 0);
+      }
+      // Update local copy of props.showAlertModal to compare them later if necessary
+      showModal.current = props.showAlertModal;
+    };
 
-  componentDidUpdate() {
-    // Check if modal should be open after updating the component
-    this.openModal();
-  }
+    openModal();
+  }, [props.showAlertModal]);
 
-  openModal = () => {
-    // Animate the modal opening
-    this.modalBody = document.getElementById("alert-modal-body");
-    this.modalBg = document.getElementById("alert-modal-background");
-    this.body = document.getElementsByTagName("body")[0];
-    // Check this.props.showAlertModal (bool), the variable that decides whether the modal is open
-    // and check if this.props.showAlertModal has been changed (if not, there's no need for the animation)
-    if (
-      this.props.showAlertModal &&
-      this.showModal !== this.props.showAlertModal
-    ) {
-      // Get the necessary elements
-      // Timeout needed for the animations to work, for elements to fully render / load
-      setTimeout(() => {
-        this.modalBody.style.transform = "";
-        this.modalBody.style.opacity = "1";
-        this.modalBg.style.opacity = "1";
-        this.body.style.overflow = "hidden";
-        // If the page is in mobile mode, remove overscroll behavior
-        // which controls refreshing the page on pull down
-        if (window.innerWidth < this.mobileWidth) {
-          this.body.style.overscrollBehavior = "hidden";
-        }
-      }, 0);
-    }
-    // Update local copy of this.props.showAlertModal to compare them later if necessary
-    this.showModal = this.props.showAlertModal;
-  };
-
-  closeModal = () => {
+  const closeModal = () => {
     // Animate the modal closing
     // Check if the page is in desktop or mobile mode - there are different animations
-    if (window.innerWidth >= this.mobileWidth) {
+    if (window.innerWidth >= mobileWidth.current) {
       // Desktop
-      this.modalBody.style.transform = "translateY(20px)";
+      modalBody.current.style.transform = "translateY(20px)";
     } else {
       // Mobile
-      this.modalBody.style.transform = "translateY(50vh)";
-      this.body.style.overscrollBehavior = "";
+      modalBody.current.style.transform = "translateY(50vh)";
+      body.current.style.overscrollBehavior = "";
     }
     // Animations that are the same in both desktop and mobile modes
-    this.modalBody.style.opacity = "0";
-    this.modalBg.style.opacity = "0";
-    this.body.style.overflow = "";
-    // Close the modal (change this.props.showAlertModal to false)
+    modalBody.current.style.opacity = "0";
+    modalBg.current.style.opacity = "0";
+    body.current.style.overflow = "";
+    // Close the modal (change props.showAlertModal to false)
     // after the animation is finished (200ms)
     setTimeout(() => {
-      this.props.toggleAlertModal(false);
+      props.toggleAlertModal(false);
     }, 200);
   };
 
-  startSwipe = (e) => {
+  const startSwipe = (e) => {
     // Save the first touch position
-    this.firstTouchY = e.touches[0].clientY;
+    firstTouchY = e.touches[0].clientY;
     // Remove the transition time for modal body so the swipe doesn't have latency
-    this.modalBody.style.transition = "0s";
+    modalBody.current.style.transition = "0s";
   };
 
-  moveSwipe = (e) => {
+  const moveSwipe = (e) => {
     // Check if the modal is in its highest position (in which it can't go further up)
-    if (e.touches[0].clientY > this.firstTouchY) {
+    if (e.touches[0].clientY > firstTouchY) {
       // Move the modal with the touch (relative to the initial touch)
-      this.modalBody.style.transform = `translateY(${
-        e.touches[0].clientY - this.firstTouchY
+      modalBody.current.style.transform = `translateY(${
+        e.touches[0].clientY - firstTouchY
       }px)`;
     } else {
       // If the current touch is higher than the initial touch,
       // just set the modal to the highest position possible
-      this.modalBody.style.transform = `translateY(0px)`;
+      modalBody.current.style.transform = `translateY(0px)`;
     }
   };
 
-  endSwipe = (e) => {
+  const endSwipe = (e) => {
     // Reset the modal's transition
-    this.modalBody.style.transition = "";
+    modalBody.current.style.transition = "";
     // Check if final touch is lower than the fist touch by more than 20 pixels,
     // if is - close the modal
     // if isn't - reset the modal to the default position
-    if (e.changedTouches[0].clientY - this.firstTouchY > 20) {
-      this.closeModal();
+    if (e.changedTouches[0].clientY - firstTouchY > 20) {
+      closeModal();
     } else {
-      this.modalBody.style.transform = `translateY(0px)`;
+      modalBody.current.style.transform = `translateY(0px)`;
     }
   };
 
-  render() {
-    // Don't render the modal if this.props.showAlertModal is false
-    if (!this.props.showAlertModal) return null;
+  // Don't render the modal if props.showAlertModal is false
+  if (!props.showAlertModal) return null;
 
-    return (
+  return (
+    <div
+      id="alert-modal"
+      onTouchStart={startSwipe}
+      onTouchMove={moveSwipe}
+      onTouchEnd={endSwipe}
+    >
       <div
-        id="alert-modal"
-        onTouchStart={this.startSwipe}
-        onTouchMove={this.moveSwipe}
-        onTouchEnd={this.endSwipe}
+        id="alert-modal-background"
+        onClick={closeModal}
+        style={{
+          opacity: "0",
+        }}
+      ></div>
+      <div
+        id="alert-modal-body"
+        style={{
+          transform:
+            window.innerWidth >= mobileWidth.current
+              ? "translateY(-20px)"
+              : "translateY(50vh)",
+          opacity: "0",
+        }}
       >
+        {props.title ? <div id="alert-modal-title">{props.title}</div> : null}
+        {props.subtitle ? (
+          <div id="alert-modal-subtitle">{props.subtitle}</div>
+        ) : null}
         <div
-          id="alert-modal-background"
-          onClick={this.closeModal}
+          id="alert-modal-buttons"
           style={{
-            opacity: "0",
-          }}
-        ></div>
-        <div
-          id="alert-modal-body"
-          style={{
-            transform:
-              window.innerWidth >= this.mobileWidth
-                ? "translateY(-20px)"
-                : "translateY(50vh)",
-            opacity: "0",
+            flexDirection: props.buttons
+              ? props.buttons.length > 2
+                ? "column"
+                : ""
+              : "",
+            margin: props.buttons
+              ? props.buttons.length > 2
+                ? "12px 12px 0 12px"
+                : ""
+              : "",
           }}
         >
-          {this.props.title ? (
-            <div id="alert-modal-title">{this.props.title}</div>
-          ) : null}
-          {this.props.subtitle ? (
-            <div id="alert-modal-subtitle">{this.props.subtitle}</div>
-          ) : null}
-          <div
-            id="alert-modal-buttons"
-            style={{
-              flexDirection: this.props.buttons
-                ? this.props.buttons.length > 2
-                  ? "column"
-                  : ""
-                : "",
-              margin: this.props.buttons
-                ? this.props.buttons.length > 2
-                  ? "12px 12px 0 12px"
-                  : ""
-                : "",
-            }}
-          >
-            {this.props.buttons
-              ? this.props.buttons.map((button) => {
-                  return (
-                    <button
-                      className="alert-modal-button"
-                      onClick={(e) => {
-                        if (button.onClick) button.onClick(e);
-                        if (
-                          !this.props.dontCloseAfterClick &&
-                          !button.dontCloseAfterClick
-                        )
-                          this.closeModal();
-                      }}
-                      key={button.name}
-                    >
-                      {button.name}
-                    </button>
-                  );
-                })
-              : null}
-            <button className="alert-modal-button" onClick={this.closeModal}>
-              Cancel
-            </button>
-          </div>
+          {props.buttons
+            ? props.buttons.map((button) => {
+                return (
+                  <button
+                    className="alert-modal-button"
+                    onClick={(e) => {
+                      if (button.onClick) button.onClick(e);
+                      if (
+                        !props.dontCloseAfterClick &&
+                        !button.dontCloseAfterClick
+                      )
+                        closeModal();
+                    }}
+                    key={button.name}
+                  >
+                    {button.name}
+                  </button>
+                );
+              })
+            : null}
+          <button className="alert-modal-button" onClick={closeModal}>
+            Cancel
+          </button>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
